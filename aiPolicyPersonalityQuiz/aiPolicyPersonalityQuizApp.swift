58c38849da51adccf66553772f03331be0f5432d
Anne Do <doanneda@gmail.com>
2024-05-27 16:22:20 -0700

    Initial Commit

---
+++ aiPolicyPersonalityQuiz/aiPolicyPersonalityQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiPolicyPersonalityQuizApp.swift
+//  aiPolicyPersonalityQuiz
+//
+//  Created by Scholar on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct aiPolicyPersonalityQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
